4e1794486262fd3699e03981084943ba5099f8d0
Ryan Housand <rhousand@tryondirect.com>
2021-12-14 15:05:54 -0500

    expose podinfo

--- expose.sh
+++ expose.sh
@@ -1,3 +1,5 @@
+export DOMAIN="trinity-healthware.com" && \
+cat << EOF | tee ingress/proxy.yaml
 apiVersion: projectcontour.io/v1
 kind: HTTPProxy
 metadata:
@@ -7,9 +9,10 @@ metadata:
     fluxcd.io/ignore: "false"
 spec:
   virtualhost:
-    fqdn: podinfo.trinity-healthware.com
+    fqdn: podinfo.${DOMAIN}
     tls:
       secretName: cert
   includes:
     - name: podinfo
       namespace: demo
+EOF
